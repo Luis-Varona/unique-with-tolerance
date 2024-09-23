b403ee9e94fd22558382857a07ba386611758ac6
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-23 16:31:35 -0300

    Renamed test variable names

--- C++/tests/main.cpp
+++ C++/tests/main.cpp
@@ -10,30 +10,30 @@ double test_arr[n];
 
 int main() {
     double *test_arr = test_array();
-    UniqueTolArray unique_tol_array = uniquetol(test_arr, n);
-    int k = unique_tol_array.num_unique;
+    UniqueTolArray uniquetol_array = uniquetol(test_arr, n);
+    int k = uniquetol_array.num_unique;
     
     printf("arr_unique:\n");
     for (int i = 0; i < k; i++) {
         printf("%d: ", i);
-        printf("%f\n", unique_tol_array.arr_unique[i]);
+        printf("%f\n", uniquetol_array.arr_unique[i]);
     }
     
     printf("\n\nindices_unique:\n");
     for (int i = 0; i < k; i++) {
         printf("%d: ", i);
-        printf("%d\n", unique_tol_array.indices_unique[i]);
+        printf("%d\n", uniquetol_array.indices_unique[i]);
     }
     
     printf("\n\ninverse_unique:\n");
     for (int i = 0; i < n; i++) {
         printf("%d: ", i);
-        printf("%d\n", unique_tol_array.inverse_unique[i]);
+        printf("%d\n", uniquetol_array.inverse_unique[i]);
     }
     
     printf("\n\ncounts_unique:\n");
     for (int i = 0; i < k; i++) {
         printf("%d: ", i);
-        printf("%d\n", unique_tol_array.counts_unique[i]);
+        printf("%d\n", uniquetol_array.counts_unique[i]);
     }
 }
