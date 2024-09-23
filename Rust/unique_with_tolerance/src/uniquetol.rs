3124d4f01dcdbdf726ea0233570aa8d32728b952
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-23 17:21:44 -0300

    Finished 1D array Rust algorithm and added prototype tests (will move to other module later)

--- Rust/unique_with_tolerance/src/uniquetol.rs
+++ Rust/unique_with_tolerance/src/uniquetol.rs
@@ -17,32 +17,96 @@ pub fn uniquetol(arr: &[f64], args: UniqueTolArgs) -> UniqueTolArray {
     }
     
     let n = arr.len();
-    if n == 0 {
-        return UniqueTolArray {
-            arr_unique: Vec::new(),
-            indices_unique: Vec::new(),
-            inverse_unique: Vec::new(),
-            counts_unique: Vec::new(),
+    let mut out = UniqueTolArray {
+        arr_unique: Vec::new(),
+        indices_unique: Vec::new(),
+        inverse_unique: Vec::new(),
+        counts_unique: Vec::new(),
+    };
+    
+    if n > 0 {
+        let perm_sorted = sortperm(arr);
+        let mut arr_sorted = Vec::with_capacity(n);
+        let mut c = arr[perm_sorted[0]];
+        arr_sorted.push(c);
+        
+        let mut indices_unique = Vec::with_capacity(n);
+        indices_unique.push(0);
+        let mut num_unique = 1;
+        
+        for i in 1..n {
+            let next = arr[perm_sorted[i]];
+            arr_sorted.push(next);
+            
+            if !isapprox(c, next, Tols { atol: args.atol, rtol: args.rtol }) {
+                c = next;
+                indices_unique.push(i);
+                num_unique += 1;
+            }
+        }
+        
+        let mut arr_unique = Vec::with_capacity(num_unique);
+        let mut inverse_unique = vec![0; n];
+        let mut counts_unique = Vec::with_capacity(num_unique);
+        
+        indices_unique = indices_unique.into_boxed_slice().into_vec();
+        let index_last = indices_unique[num_unique - 1];
+        let count_last = n - index_last;
+        
+        for j in 0..count_last {
+            inverse_unique[perm_sorted[index_last + j]] = num_unique - 1;
+        }
+        
+        for i in 0..(num_unique - 1) {
+            let index = indices_unique[i];
+            let count = indices_unique[i + 1] - index;
+            counts_unique.push(count);
+            
+            for j in 0..count {
+                inverse_unique[perm_sorted[index + j]] = i;
+            }
+        }
+        
+        counts_unique.push(count_last);
+        
+        if use_highest {
+            for i in 0..(num_unique) {
+                indices_unique[i] = perm_sorted[indices_unique[i]];
+            }
+            
+            indices_unique[num_unique - 1] = n - 1;
+        }
+        
+        for i in 0..num_unique {
+            let index = perm_sorted[indices_unique[i]];
+            arr_unique.push(arr_sorted[index]);
+            indices_unique[i] = index;
+        }
+        
+        out = UniqueTolArray {
+            arr_unique: arr_unique,
+            indices_unique: indices_unique,
+            inverse_unique: inverse_unique,
+            counts_unique: counts_unique,
         };
     }
-    else {
-        unimplemented!();
-    }
+    
+    return out;
 }
 
 //
 pub struct UniqueTolArray {
-    arr_unique: Vec<f64>,
-    indices_unique: Vec<usize>,
-    inverse_unique: Vec<usize>,
-    counts_unique: Vec<u32>,
+    pub arr_unique: Vec<f64>,
+    pub indices_unique: Vec<usize>,
+    pub inverse_unique: Vec<usize>,
+    pub counts_unique: Vec<usize>,
 }
 
 //
 pub struct UniqueTolArgs {
-    atol: f64,
-    rtol: f64,
-    occurrence: &'static str,
+    pub atol: f64,
+    pub rtol: f64,
+    pub occurrence: &'static str,
 }
 
 //
