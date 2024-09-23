780e2c6d940f8482d39197d9720551da7d4fc678
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-23 09:53:01 -0300

    Reconfigured repo settings and Rust layout

--- Rust/unique_with_tolerance/target/debug/deps/unique_with_tolerance-6a0b424b7d6c44a6.d
+++
@@ -1,5 +0,0 @@
-/Users/luismbvarona/GitHub/unique-with-tolerance/Rust/unique_with_tolerance/target/debug/deps/libunique_with_tolerance-6a0b424b7d6c44a6.rmeta: src/main.rs
-
-/Users/luismbvarona/GitHub/unique-with-tolerance/Rust/unique_with_tolerance/target/debug/deps/unique_with_tolerance-6a0b424b7d6c44a6.d: src/main.rs
-
-src/main.rs:
