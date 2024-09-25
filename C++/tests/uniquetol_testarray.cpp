590ed140c746a9a953da547c7a31eda638fc21fe
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-25 08:56:30 -0300

    Finished prototype rand_testarray function in C++

--- C++/tests/uniquetol_testarray.cpp
+++ C++/tests/uniquetol_testarray.cpp
@@ -17,9 +17,10 @@ double *rand_testarray(int k, int max_count) {
     int shift_min = -334 + rand() % 248;
     int shift_max = 87 + rand() % 248;
     
+    int n;
     int counts_initial[k];
     int values_initial[k];
-    double *diffs[k]; // Correct syntax?
+    double (*diffs[k]);
     
     for (int i = 0; i < num_ones; i++) {
         counts_initial[i] = 1;
@@ -30,11 +31,36 @@ double *rand_testarray(int k, int max_count) {
     }
     
     for (int i = 0; i < k; i++) {
+        int ct = counts_initial[i];
+        n += ct;
+        
         values_initial[i] = value_min + rand() % (value_max - value_min + 1);
         values_initial[i] += shift_min + rand() % (shift_max - shift_min + 1);
+        diffs[i] = new double[ct];
+        
+        for (int j = 0; j < ct; j++) {
+            diffs[i][j] = rand_double(-1e6, 1e6);
+        }
     }
     
-    // ADD LATER
+    double *testarray = new double[n];
+    
+    for (int i = 0; i < n; i ++) {
+        int val = values_initial[i];
+        int ct = counts_initial[i];
+        double *diffs_chunk = diffs[i];
+        
+        for (int j = 0; j < ct; j ++) {
+            testarray[i + j] = val + diffs_chunk[j];
+        }
+        
+        free(diffs_chunk);
+        free(diffs[i]);
+        diffs_chunk = NULL;
+        diffs[i] = NULL;
+    }
+    
+    return testarray;
 }
 
 //
