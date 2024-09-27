ca1af3cc1af8cb1ea412725f2db1ad0d6b5abb0a
Luis Miguel Varona <lbvarona@mta.ca>
2024-09-27 00:38:13 -0300

    Updated Rust system benchmark

--- Rust/tests/src/proto_bench.rs
+++ Rust/tests/src/proto_bench.rs
@@ -6,15 +6,18 @@ use testarray::ARR;
 use uniquetol::{UniqueTolArgs, uniquetol};
 
 fn main() {
+    let arr_ref = &ARR;
     let mut num: i32 = 10;
     num = num.pow(5);
     println!("Number of iterations: {}", num);
-    let now = Instant::now();
+    let start = Instant::now();
     
     for _ in 0..num {
-        uniquetol(&ARR, UniqueTolArgs::default());
+        uniquetol(arr_ref, UniqueTolArgs::default());
     }
+    // uniquetol(arr_ref, UniqueTolArgs::default());
     
-    let elapsed_time = now.elapsed();
+    let elapsed_time = start.elapsed();
     println!("Elapsed time: {:?}", elapsed_time.as_secs_f64());
+    // println!("Elapsed time: {:?}", elapsed_time.as_nanos());
 }
